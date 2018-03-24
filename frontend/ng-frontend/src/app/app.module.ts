170b79f38d7d10084c3ef34d7560035229f9e627
Samuel Tumewa <stumewa@yahoo.com>
2018-03-24 15:44:09 +1100

    working commit

--- frontend/ng-frontend/src/app/app.module.ts
+++ frontend/ng-frontend/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './components/app.component';
+import { UserComponent } from './user/user.component';
+import { EventComponent } from './event/event.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { ProfileWidgetComponent } from './profile-widget/profile-widget.component';
+import { FilterComponent } from './filter/filter.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    EventComponent,
+    OrganizationComponent,
+    ProfileWidgetComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/ng-frontend/src/app/event.ts
@@ -0,0 +1,9 @@
+export class Event{
+  name: string;
+  startDate: Date;
+  endDate: Date;
+  desc: string;
+  location: number[];
+  commitmentDesc: string;
+  commitment: number;
+}
