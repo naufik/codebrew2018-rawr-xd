befe4de3bc87bb417340da3008d1d65f15230ee2
naufik <nauffik@gmail.com>
2018-03-25 07:18:14 +1100

    dynamic search added

--- frontend/ng-frontend/src/app/components/app.component.ts
+++ frontend/ng-frontend/src/app/components/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	
+	title = 'app';
+	public searchState = {query: ""};
+	public searchMode = false;
+
+	public handleSearch(event) {
+		this.title = event.query;
+		this.searchState = event;
+		this.searchMode = (this.searchState.query !== '');
+	}
 }
