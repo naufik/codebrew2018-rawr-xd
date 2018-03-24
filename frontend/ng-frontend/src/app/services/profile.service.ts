54458db9f78626a4055723e90519b8bce6ae32d8
naufik <nauffik@gmail.com>
2018-03-24 14:42:05 +1100

    fake user services added

--- frontend/ng-frontend/src/app/services/profile.service.ts
+++ frontend/ng-frontend/src/app/services/profile.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import * as Faker from 'faker';
+
+interface User {
+	userid: number,
+	name: {
+		first: string,
+		last: string
+	},
+	register: Date
+}
 
 @Injectable()
 export class ProfileService {
+	private data: User[] = [];
+	
+	constructor() { 
+		for(let i = 0; i < 100; ++i) {
+			this.data.push({
+				userid: i,
+				name: {
+					first: Faker.name.firstName(),
+					last: Faker.name.lastName()
+				},
+				register: Faker.date.past()
+			});
+		}
+	}
 
-  constructor() { }
+	public getUser(id: number) {
+		return new Promise((resolve, reject) => {
+			if (id < this.data.length) {
+				resolve(this.data[id]);
+			} else {
+				reject(new Error("User with that ID not found."));
+			}
+		});
+	}
 
+	public getAllUsers() {
+		return new Promise((resolve, reject) => {
+			resolve(this.data);
+		});
+	}
 }
