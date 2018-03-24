425e9a63aa47a8514ce1fd5de00ab3bdaae1dd90
naufik <nauffik@gmail.com>
2018-03-24 19:24:41 +1100

    added temporary text reader

---
+++ frontend/ng-frontend/src/app/services/textdb.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class JSONService {
+	private http: HttpClient;
+
+	constructor(http: HttpClient) {
+		this.http = http;
+	}
+
+	public read(url: string) {
+		return new Promise((resolve, reject) => {
+			this.http.get(url).subscribe((data) => {
+				resolve(data)
+			});
+		});
+	}
+}
