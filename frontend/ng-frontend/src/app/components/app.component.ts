efb28ff820c953f251b3abeba776aa6446522453
ChuanyuanLiu <liuchuanyuan@gmail.com>
2018-03-25 14:53:21 +1100

    temp

--- frontend/ng-frontend/src/app/components/app.component.ts
+++ frontend/ng-frontend/src/app/components/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-	title = 'app';
+	title = 'Volunteeraptor';
 	public searchState = {
     	query: '',
     	region: true,
